6bb44ff58bd9338da9ea8af931bb7368662ec7b5
dmchoi <david5760@naver.com>
2021-11-04 20:20:27 +0900

    로그아웃 기능 추가

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
+const { auth } = require('./middleware/auth')
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 
@@ -20,7 +21,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello'))
 
-app.post('/register', (req, res) => {
+app.post('/api/register', (req, res) => {
     //회원가입시 필요한 정보를 클라이언트에서 가져오면 
     //DB에 넣어준다.
     const user = new User(req.body)
@@ -33,7 +34,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/login', (req, res) => {
     //요청된 아이디를 DB에 있는지 찾는다
     User.findOne({ id: req.body.id }, (err, user) => {
         if (!user) {
@@ -59,8 +60,29 @@ app.post('/login', (req, res) => {
             })
         })
     })
-
 })
 
+
+app.get('/api/users/auth', auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true, //role = 1 일반 유저, 0 관리자
+        isAuth: true,
+        name: req.user.name,
+        role: req.user.role,
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" }
+        , (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
